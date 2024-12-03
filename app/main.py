e45152313f75f9ebccdcdca748bd711437a95273
Anand Vardhan <anandvar669@gmail.com>
2024-12-03 20:10:23 +0530

    [any message]

--- app/main.py
+++ app/main.py
@@ -9,6 +9,9 @@ def main():
     #
     server_socket = socket.create_server(("localhost", 6379), reuse_port=True)
     server_socket.accept() # wait for client
+    comm, tt = server_socket.accept()  # wait for client
+
+    comm.sendall(b"+PONG\r\n")
 
 
 if __name__ == "__main__":
